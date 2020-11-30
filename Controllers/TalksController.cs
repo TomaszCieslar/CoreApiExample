8dbc1993e03e26e9aefa581dca65e2198fe26bbd
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2020-11-30 04:59:39 +0100

    Add project files.

---
+++ Controllers/TalksController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _respository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository respository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            _respository = respository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await _respository.GetTalksByMonikerAsync(moniker);
+                return _mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get talks");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _respository.GetTalkByMonikerAsync(moniker, id);
+                return _mapper.Map<TalkModel[]>(talk);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get talks");
+            }
+        }
+
+
+    }
+}
